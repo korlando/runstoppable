9695016007f4b5faddedbb838da33d97ce280aa2
Keith Orlando <korlando@mit.edu>
2017-04-15 14:22:23 -0400

    display start date on SingleRunPage

--- src/components/SingleRunPage.js
+++ src/components/SingleRunPage.js
@@ -20,7 +20,10 @@ export default class SingleRunPage extends Component {
 
     return (
       <div className="page-container">
-        <h2>{run.location}</h2>
+        <div className="flexbox align-items-baseline">
+          <h2 style={{marginRight: '10px'}}>{run.location}</h2>
+          <h3 style={{color: '#747e95'}}>{run.start.format('MMM D h:mm a')}</h3>
+        </div>
         <PaceChart runId={run.id}/>
       </div>
     );
