066839990a9c458d990a6ca9f35b478491aaf98d
Keith Orlando <korlando@mit.edu>
2017-04-15 14:34:29 -0400

    add sorted runs selector

--- src/selectors/getMostRecentRun.js
+++ src/selectors/getMostRecentRun.js
@@ -1,25 +1,13 @@
 import { createSelector } from 'reselect';
+import getSortedRunsByStart from './getSortedRunsByStart';
 
 const getRunMap = (state) => state.run.runMap;
 
 export default createSelector(
-  [ getRunMap ],
-  (runMap) => {
-    const sortedIds = Object.keys(runMap).sort((a, b) => {
-      const runA = runMap[a];
-      const runB = runMap[b];
-
-      if(runA.start.isBefore(runB.start)) {
-        return 1;
-      }
-      if(runB.start.isBefore(runA.start)) {
-        return -1;
-      }
-      return a - b;
-    });
-
-    if(sortedIds.length) {
-      return runMap[sortedIds[0]];
+  [ getSortedRunsByStart ],
+  (sortedRuns) => {
+    if(sortedRuns.length) {
+      return sortedRuns[0];
     }
 
     return null;
