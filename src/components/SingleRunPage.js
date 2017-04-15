12305249a726893df932962cac0ea92b0bc74fcf
Keith Orlando <korlando@mit.edu>
2017-04-15 14:25:49 -0400

    smarter date parsing

--- src/components/SingleRunPage.js
+++ src/components/SingleRunPage.js
@@ -18,11 +18,21 @@ export default class SingleRunPage extends Component {
     const { run } = this.props;
     if(!run) return null;
 
+    let dateFormat = 'MMM D, h:mm a';
+    const thisYear = new Date().getFullYear();
+    if(run.start.year() !== thisYear) {
+      dateFormat = 'MMM D, YYYY, h:mm a';
+    }
+
     return (
       <div className="page-container">
         <div className="flexbox align-items-baseline">
-          <h2 style={{marginRight: '10px'}}>{run.location}</h2>
-          <h3 style={{color: '#747e95'}}>{run.start.format('MMM D h:mm a')}</h3>
+          <h2 style={{marginRight: '10px'}}>
+            {run.location}
+          </h2>
+          <h3 style={{color: '#747e95'}}>
+            {run.start.format(dateFormat)}
+          </h3>
         </div>
         <PaceChart runId={run.id}/>
       </div>
