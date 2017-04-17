8d847a74c92982a28e79f73d51a3683dde0e5e69
Ethan Witt <epw@@mit.edu>
2017-04-17 03:21:23 -0400

    added runboxes, compare/cancel buttons to modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { toggleModal } from '../../util'
+import getSortedRunsByStart from '../../selectors/getSortedRunsByStart';
+import RunBox from '../RunBox';
 
 const mapStateToProps = (state) => {
   return {
-
+    runs: getSortedRunsByStart(state),
+    checkedRuns: state.checkedRuns
   };
 };
 
@@ -11,13 +15,31 @@ const mapStateToProps = (state) => {
 export default class Modal extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      checkedRuns: [],
+      runs: []
+    }
   };
 
   render() {
+    const { runs, checkedRuns } = this.props;
 
     return (
-      <div className={'modal-custom'}>
-      
+      <div className="modal-custom">
+        <div className="modal-header">
+          <h3 className="text-center">Select runs to compare</h3>
+        </div>
+        <div className="modal-scroll-view">
+          { runs.map((run) => {
+            return (
+              <RunBox key={run.id} run={run}/>
+            );
+          })}
+        </div>
+        <div className="modal-footer">
+          <button>Compare</button>
+          <button onClick={ toggleModal }>Cancel</button>
+        </div>
       </div>
     );
   };
