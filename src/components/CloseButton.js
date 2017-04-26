d87bd8bf79df287beb15c83d80312d8a3910f90d
Keith Orlando <korlando@mit.edu>
2017-04-26 14:22:49 -0400

    make hamburger icon wider

--- src/components/CloseButton.js
+++ src/components/CloseButton.js
@@ -6,13 +6,13 @@ export default (props) => {
       className={`menu-icon ${props.className || ''}`}
       onClick={props.onClick}>
       <svg className="line">
-        <line x1="2" x2="18" y1="2" y2="2"/>
+        <line x1="2" x2="22" y1="2" y2="2"/>
       </svg>
       <svg className="line">
-        <line x1="2" x2="18" y1="2" y2="2"/>
+        <line x1="2" x2="22" y1="2" y2="2"/>
       </svg>
       <svg className="line">
-        <line x1="2" x2="18" y1="2" y2="2"/>
+        <line x1="2" x2="22" y1="2" y2="2"/>
       </svg>
     </button>
   );
