76c9ee19ac232b8219c8d9d58234c07d9beb79fa
Ethan Witt <epw@@mit.edu>
2017-04-27 19:27:41 -0400

    disabled compare when no runs selected, fixed BigTime font weight

--- src/components/BigTime.js
+++ src/components/BigTime.js
@@ -7,7 +7,8 @@ export default (props) => {
         style={{ lineHeight: '1' }}>
         <div style={{
           fontSize: '40px',
-          marginRight: '3px'
+          marginRight: '3px',
+          fontWeight: '300'
         }}>{props.hours}</div>
         <div className="text-light" style={{
           fontSize: '18px',
@@ -15,7 +16,8 @@ export default (props) => {
         }}>h</div>
         <div style={{
           fontSize: '40px',
-          marginRight: '3px'
+          marginRight: '3px',
+          fontWeight: '300'
         }}>{props.minutes}</div>
         <div className="text-light" style={{
           fontSize: '18px',
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router';
 import { connect } from 'react-redux';
 import { toggleModal } from '../../util'
 import { Link } from 'react-router-dom';
@@ -12,69 +13,74 @@ const mapStateToProps = (state) => {
   };
 };
 
-@connect(mapStateToProps)
-export default class Modal extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      checkedRuns: [],
-      allChecked: false
+export default withRouter(
+  @connect(mapStateToProps)
+  class Modal extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        checkedRuns: [],
+        allChecked: false
+      };
     };
-  };
 
-  render() {
-    const { runs } = this.props;
-    const { checkedRuns, allChecked } = this.state;
+    render() {
+      const { runs, history } = this.props;
+      const { checkedRuns, allChecked } = this.state;
 
-    return (
-      <div className="modal-custom" onClick={e => e.stopPropagation()}>
-        <div className="modal-top flexbox align-items-center">
-          <h2 className="flex1">Select runs to compare</h2>
-          <div className="flex0">
-            <Checkbox
-              checked={allChecked}
-              onCheckChange={(checked) => {
-                const newCheckedRuns = checked ?
-                  runs.reduce((arr, run) => [...arr, run.id], []) :
-                  [];
-                this.setState({
-                  allChecked: checked,
-                  checkedRuns: newCheckedRuns
-                });
-              }}/>
+      return (
+        <div className="modal-custom" onClick={e => e.stopPropagation()}>
+          <div className="modal-top flexbox align-items-center">
+            <h2 className="flex1">Select runs to compare</h2>
+            <div className="flex0">
+              <Checkbox
+                checked={allChecked}
+                onCheckChange={(checked) => {
+                  const newCheckedRuns = checked ?
+                    runs.reduce((arr, run) => [...arr, run.id], []) :
+                    [];
+                  this.setState({
+                    allChecked: checked,
+                    checkedRuns: newCheckedRuns
+                  });
+                }}/>
+            </div>
+          </div>
+          <div className="modal-scroll-view">
+            { runs.map((run) => {
+              return (
+                <RunBox key={run.id}
+                  run={run}
+                  checkable={true}
+                  checked={checkedRuns.includes(run.id)}
+                  onCheckChange={(checked) => {
+                    if(checked) {
+                      this.setState({
+                        checkedRuns: [...checkedRuns, run.id]
+                      });
+                    } else {
+                      this.setState({
+                        checkedRuns: checkedRuns.filter(id => {
+                          return id !== run.id;
+                        })
+                      });
+                    }
+                  }}/>
+              );
+            })}
+          </div>
+          <div className="modal-footer">
+            <button className="btn btn-primary"
+              disabled={checkedRuns.length < 2}
+              onClick={ function(){
+                toggleModal();
+                var linkTo = checkedRuns.reduce((str, id, index) => str + (index == 0 ? "" : ",") + id , "/compare/");
+                history.push(linkTo); }
+              }>Compare</button>
+            <button className="btn btn-default" onClick={ toggleModal }>Cancel</button>
           </div>
         </div>
-        <div className="modal-scroll-view">
-          { runs.map((run) => {
-            return (
-              <RunBox key={run.id}
-                run={run}
-                checkable={true}
-                checked={checkedRuns.includes(run.id)}
-                onCheckChange={(checked) => {
-                  if(checked) {
-                    this.setState({
-                      checkedRuns: [...checkedRuns, run.id]
-                    });
-                  } else {
-                    this.setState({
-                      checkedRuns: checkedRuns.filter(id => {
-                        return id !== run.id;
-                      })
-                    });
-                  }
-                }}/>
-            );
-          })}
-        </div>
-        <div className="modal-footer">
-          <Link className="btn btn-primary"
-            disabled={checkedRuns.length < 2}
-            onClick={ toggleModal }
-            to={checkedRuns.reduce((str, id, index) => str + (index == 0 ? "" : ",") + id , "/compare/")}>Compare</Link>
-          <button className="btn btn-default"onClick={ toggleModal }>Cancel</button>
-        </div>
-      </div>
-    );
-  };
-};
+      );
+    };
+  }
+);
