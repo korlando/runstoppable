b122640caf793d301bc43cccb4869f02f1864afa
Ethan Witt <epw@@mit.edu>
2017-05-03 19:18:19 -0400

    made hamburger button not highlight on mouse click

--- src/components/CloseButton.js
+++ src/components/CloseButton.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default (props) => {
-  return (
-    <button
-      className={`menu-icon ${props.className || ''}`}
-      onClick={props.onClick}>
-      <svg className="line">
-        <line x1="2" x2="22" y1="2" y2="2"/>
-      </svg>
-      <svg className="line">
-        <line x1="2" x2="22" y1="2" y2="2"/>
-      </svg>
-      <svg className="line">
-        <line x1="2" x2="22" y1="2" y2="2"/>
-      </svg>
-    </button>
-  );
+export default class PaceChart extends Component {
+  constructor(props) {
+    super(props);
+  };
+
+  render() {
+    const { onClick, className } = this.props;
+    
+    return (
+      <button
+        className={`menu-icon ${className || ''}`}
+        onClick={onClick}
+        ref={node => this.closeButton = node}
+        onMouseDown={() => {
+          setTimeout(() => {this.closeButton.blur()}, 1);
+        }}>
+        <svg className="line">
+          <line x1="2" x2="22" y1="2" y2="2"/>
+        </svg>
+        <svg className="line">
+          <line x1="2" x2="22" y1="2" y2="2"/>
+        </svg>
+        <svg className="line">
+          <line x1="2" x2="22" y1="2" y2="2"/>
+        </svg>
+      </button>
+    );
+  }
 };
