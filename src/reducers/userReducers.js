8e16eda53a6a6cd959f5ba7b24f5e084f4a68ba9
Joyce Chen <chenjy@mit.edu>
2017-05-12 16:09:30 -0400

    add improved images

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -1,6 +1,6 @@
 const defaultState = {
   name: 'Ron Stoppable',
-  photo: 'https://avatars.slack-edge.com/2017-03-24/158411923920_7614b17cc53af6223f1b_72.jpg',
+  photo: 'images/default-ron.jpg',
   shoes: [],
   heightFt: '5',
   heightIn: '6',
